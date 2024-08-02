94f7d71a8428fab51b2c49203bc887ef568929c5
susanacst <susanacst098@gmail.com>
2024-08-01 19:45:14 -0600

    Clase 2 Anatomia de variables
    Clase terminada, diferencias de let y const

---
+++ c-2-anatomia-variables.js
@@ -0,0 +1,35 @@
+// CLASE 2
+
+// Una variable es un contenedor que almacena datos. Las variables pueden ser nombradas o etiquetadas 
+// para identificar lo que tienen dentro.
+
+// "let" es una palabra clave que se utiliza al inicio de la declaración de una variable.
+
+// ***COSAS A TOMAR EN CUENTA PARA LA CREACIÓN DE UNA VARIABLE*** //
+// 1. Utilizar nombres descriptivos, aunque sean largos, pero que se puedan entender.
+// 2. Evitar ambigüedades o abreviaciones.
+
+// **ABREVIACIONES PERMITIDAS** //
+let primerTrasteoDeAndy = 'Woody'; // Nombres descriptivos para mayor claridad
+let urlDelUsuario = 'www.google.com';
+let idDelUsuario = '123456';
+let idUsuario = '123456'; // Evitar abreviaciones a menos que sean comunes y entendibles
+
+// BUENAS PRÁCTICAS //
+// 1. Tener un área de declaración de variables (normalmente al inicio del archivo).
+// 2. Usar punto y coma (;) al final de cada declaración.
+
+let cajaDeAndy = 'Woody'; // Variable creada para almacenar el nombre de un juguete
+console.log(cajaDeAndy); // Visualizar el contenido de la variable en la consola
+
+// **PARTES DE UNA VARIABLE** //
+// Declaración: "let cajaDeAndy"
+// Asignación: "='Woody'"
+
+// **LET Y CONST** //
+// "let" se utiliza para variables que pueden cambiar:
+let contador = 0; // Valor inicial
+contador = contador + 1; // La variable cambia con el tiempo
+
+// "const" se utiliza para variables cuyo valor no cambiará:
+const pi = 3.14; // El valor de pi es constante y no cambia
