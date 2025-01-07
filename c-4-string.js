1a8d04dbce607ec75b7cc6b97209ffb7fe3503e0
susanacst <susanacst098@gmail.com>
2025-01-06 20:39:19 -0600

    avance clase string

---
+++ c-4-string.js
@@ -0,0 +1,12 @@
+// Las cadenas de caracteres, o strings son un tipo de dato fundamental en JavaScript, 
+// representan secuencias de caracteres y se utilizan parar almacenar y manipular texto
+// como nombres, frases, codigo HTML o cualquier tipo de información textual.
+// Existen dos formas de crearlos
+//1. Literales de Cadena: Son la forma más común y es utilizando comillas simples (') o comillas dobles ("") 
+// y así delimitar la secuencia de caracteres
+const saludo = "Henlo mundo!";
+const mensaje = 'Hoy estoy orgullosa de mí.'
+
+// 2. Plantillas de cadena
+// Son una forma más moderna y flexible de crearlas. 
+// Permiten insertar expresiones  dentro de la cadena utilizando el símbolo de re
